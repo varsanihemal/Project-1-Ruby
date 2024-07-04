956fd2dbb6886792f130c5279be949c3bb5b60a3
varsanihemal <hemalvarsani123@gmail.com>
2024-07-04 16:19:25 -0500

    modification of views to display data in the website

--- config/routes.rb
+++ config/routes.rb
@@ -5,25 +5,18 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "cryptocurrencies#index"
+  # Home routes
+  root "home#index"
+  get 'about', to: 'home#about'
 
-  #resource roots
-  resources :cryptocurrencies, only: [:index, :show]
-  resources :exchanges, only: [:index, :show]
-  resources :users, only: [:index, :show]
-  resources :wallets, only: [:index, :show]
-
-  # Nested routes for hierarchical navigation
-  resources :wallets do
-    resources :wallet_cryptocurrencies, only: [:index, :show]
-  end
-
-  resources :cryptocurrencies do
+  # Resource routes
+  resources :cryptocurrencies, only: [:index, :show] do
     resources :cryptocurrency_exchanges, only: [:index, :show]
   end
 
-  # About page
-  get 'about', to: 'pages#about'
-
+  resources :exchanges, only: [:index, :show]
+  resources :users, only: [:index, :show]
+  resources :wallets, only: [:index, :show] do
+    resources :wallet_cryptocurrencies, only: [:index, :show]
+  end
 end

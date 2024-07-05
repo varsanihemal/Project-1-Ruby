175e3dc287b6b7b51338d55f137b529fb6bd047a
varsanihemal <hemalvarsani123@gmail.com>
2024-07-05 01:56:59 -0500

    simple search for looking up for cryptocurrencies

--- app/controllers/cryptocurrencies_controller.rb
+++ app/controllers/cryptocurrencies_controller.rb
@@ -1,10 +1,14 @@
 class CryptocurrenciesController < ApplicationController
   def index
-    @cryptocurrencies = Cryptocurrency.all
+    @cryptocurrencies = if params[:search].present?
+                          Cryptocurrency.where("name LIKE ?", "%#{params[:search]}%")
+                        else
+                          Cryptocurrency.all
+                        end
   end
 
   def show
-    @cryptocurrency  = Cryptocurrency.find(params[:id])
+    @cryptocurrency = Cryptocurrency.find(params[:id])
     @exchanges = @cryptocurrency.exchanges
   end
 end
